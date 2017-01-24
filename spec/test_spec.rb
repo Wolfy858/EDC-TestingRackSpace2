6222b965de2da28300abb19e90cca2a42046237a
Elliot <Wolfy858@gmail.com>
2017-01-24 15:53:55 -0800

    commented out failing spec, so that Travis hopefully reports a successful build when this is pushed

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,7 +1,9 @@
 RSpec.describe "Basic math" do
+=begin
   it "can identify when simple addition is wrong" do
     expect(1 + 1).to eq(1)
   end
+=end
 
   it 'can identify when simple addition is correct' do
     expect(1 + 1).to eq(2)
